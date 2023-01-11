10eef2901f3db17a5946d612260de744d9173337
Raphael Zimmermann <raphael.zimmermann@ergon.ch>
2023-01-11 09:57:28 +0100

    Add docker compose for simple testing

--- flake.nix
+++ flake.nix
@@ -15,9 +15,10 @@
       pkgs = import nixpkgs {inherit system;};
       nodejs = pkgs.nodejs-19_x;
       start-server = pkgs.writeShellScriptBin "start-server" ''cd $REPOSITORY_ROOT/pixelpact; npm run start'';
+      start-server-docker = pkgs.writeShellScriptBin "start-server-docker" ''cd $REPOSITORY_ROOT/pixelpact; docker compose up --build'';
     in {
       devShell = pkgs.mkShellNoCC {
-        buildInputs = with pkgs; [nodejs start-server];
+        buildInputs = with pkgs; [nodejs start-server start-server-docker];
         shellHook = ''
           export REPOSITORY_ROOT=$(pwd)
         '';
