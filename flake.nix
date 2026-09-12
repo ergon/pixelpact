88790c0fcdd4dfa57fb33b5980c64e026be9e7f8
Raphael Zimmermann <mail@raphael.li>
2026-09-12 10:41:02 +0200

    Remove playwright flake input

--- flake.nix
+++ flake.nix
@@ -4,22 +4,16 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
-    playwright.url = "github:pietdevries94/playwright-web-flake/1.63.0";
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
-    playwright,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      overlay = final: prev: {
-        inherit (playwright.packages.${system}) playwright-test playwright-driver;
-      };
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [overlay];
       };
       nodejs = pkgs.nodejs_20;
       start-server = pkgs.writeShellScriptBin "start-server" ''cd $REPOSITORY_ROOT/pixelpact; npm run start'';
@@ -29,7 +23,6 @@
         buildInputs = with pkgs; [nodejs start-server start-server-docker];
         shellHook = ''
           export REPOSITORY_ROOT=$(pwd)
-          export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
           export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
           export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=true
           ln -fs "$REPOSITORY_ROOT/bin/pre-commit" "$REPOSITORY_ROOT/.git/hooks/pre-commit"
