6ca552143afe38a68d80db40d7afeea7d0718025
Raphael Zimmermann <mail@raphael.li>
2026-09-12 10:59:09 +0200

    Add chrome dependencies to run playwright on nixos

--- flake.nix
+++ flake.nix
@@ -16,6 +16,27 @@
         inherit system;
       };
       nodejs = pkgs.nodejs;
+      # Playwright's prebuilt chromium is not patchelf'ed for nix; give it its shared libs.
+      # Keep in sync with: ldd chrome-headless-shell | grep 'not found'
+      chromium-libs = pkgs.lib.makeLibraryPath (with pkgs; [
+        alsa-lib
+        at-spi2-core
+        dbus
+        expat
+        glib
+        libgbm
+        libx11
+        libxcb
+        libxcomposite
+        libxdamage
+        libxext
+        libxfixes
+        libxkbcommon
+        libxrandr
+        nspr
+        nss
+        systemd
+      ]);
       start-server = pkgs.writeShellScriptBin "start-server" ''cd $REPOSITORY_ROOT/pixelpact; npm run start'';
       start-server-docker = pkgs.writeShellScriptBin "start-server-docker" ''cd $REPOSITORY_ROOT/pixelpact; docker compose up --build'';
     in {
@@ -24,7 +45,7 @@
         shellHook = ''
           export REPOSITORY_ROOT=$(pwd)
           export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
-          export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=true
+          export LD_LIBRARY_PATH="${chromium-libs}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
           ln -fs "$REPOSITORY_ROOT/bin/pre-commit" "$REPOSITORY_ROOT/.git/hooks/pre-commit"
         '';
       };
